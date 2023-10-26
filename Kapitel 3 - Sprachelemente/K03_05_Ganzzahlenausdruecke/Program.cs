b46cc8fc89c19b56b9e6321f404a101303cd2b22
Corbian Reiser <corb.reiser@gmail.com>
2023-10-26 11:06:18 +0200

    commit

---
+++ Kapitel 3 - Sprachelemente/K03_05_Ganzzahlenausdruecke/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Bitte geben Sie eine Zahl ein: ");
+
+int inputnumber;
+int digit;
+int pos = 0;
+
+if (int.TryParse(Console.ReadLine(), out inputnumber))
+{
+    do
+    {
+        digit = inputnumber % 10;
+        inputnumber /= 10;
+        pos += 1;
+        Console.WriteLine($"postition {pos}: digit = {digit}");
+    } while (inputnumber != 0);
+}
