50e4b7e078d3ff370109fc8d2c0a44b499e27d50
Corbian Reiser <corb.reiser@gmail.com>
2023-11-16 09:21:48 +0100

    push

---
+++ Kapitel 4 - Kontrollstrukturen/K04_21_Try-Catch-Finally/Program.cs
@@ -0,0 +1,28 @@
+﻿int i = 1;
+int k = 0;
+
+int iErg;
+
+try
+{
+    iErg = i / k;
+}
+catch (DivideByZeroException divideByZeroException)
+{
+    string strError = divideByZeroException.Message;
+
+}
+catch (FormatException formatException)
+{
+    return;
+}
+catch (Exception e)
+{
+    string strError = e.Message;
+    Console.WriteLine("Exception in: " + strError);
+}
+finally
+{
+    // Todo...
+}
+Console.ReadKey();
---
+++ Kapitel 5 - Arrays/K05_07_Primzahlen/Program.cs
@@ -0,0 +1,29 @@
+﻿// Endwert einlesen
+Console.WriteLine("Bis zu welcher Zahl sollen die Primzahlen berechnet werden?");
+int max = int.Parse(Console.ReadLine());
+
+// Feld bCancel mit max logischen Werten erzeugen
+// false --> noch nicht gestrichen
+bool[] bCancel = new bool[max];
+for (int i = 0; i < max; i++)
+{
+    bCancel[i] = false;
+}
+for (int i = 2; i <= max; i++)
+{
+    // Wenn das Feld noch nicht gestrichen
+    if (bCancel[i-1] != true)
+    {
+        // Primzahl gefunden
+        Console.WriteLine(i + " ist eine Primzahl");
+    }
+    // Von gewählter Zahl aus alle Elemente durchlaufen und auf teilbarkeit ohne rest vergleichen
+    for (int j = 2 * i; j <= max; j+=i)
+    {
+        // Alle Zahlen streichen die Vielfache der Primzahl sind
+        if (j % i == 0)
+            bCancel[j-1] = true;
+    }//end for
+} //end outer for 
+
+Console.ReadKey();
