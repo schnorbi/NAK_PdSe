017a60f4681c833282ab7984adefa12a1633275c
Corbian Reiser <corb.reiser@gmail.com>
2023-12-07 09:42:23 +0100

    7.12.2023

--- OOProg-02-Beziehungen/B02_02_AutoAnhaenger/Anhaenger.cs
+++ OOProg-02-Beziehungen/B02_02_AutoAnhaenger/Anhaenger.cs
@@ -2,5 +2,40 @@ namespace B02_02_AutoAnhaenger;
 
 public class Anhaenger
 {
+    #region fields
     
+    private readonly string _marke;
+    private readonly string _modell;
+    private readonly double _gewicht;
+
+    // Assoziationsvariable
+
+    private Auto _auto;
+
+    #endregion
+
+    #region ctor
+
+    public Anhaenger(string marke, string modell, double gewicht)
+    {
+        _marke = marke;
+        _modell = modell;
+        _gewicht = gewicht;
+    }
+
+    #endregion
+
+    #region methods
+
+    // Assoziationsmethoden
+    public void Ankuppeln(Auto auto)
+    {
+        if (_auto == null)
+        {
+            _auto = auto;
+        }
+    }
+
+    #endregion
+
 }
---
+++ OOProg-02-Beziehungen/B02_02_AutoAnhaenger/Test.cs
@@ -0,0 +1,20 @@
+﻿namespace B02_02_AutoAnhaenger;
+
+class Test {
+    void Run()
+    {
+        Auto auto = new Auto("VW", "Golf", "UE-JK-250");
+
+        
+    }
+
+    static void Main()
+    {
+        Test test = new Test();
+        test.Run();
+
+        Console.ReadKey();
+
+    }
+
+}
