4887804d61c0cf3cb6e4d60a02eb16ac9c078988
Corbian Reiser <corb.reiser@gmail.com>
2023-12-07 09:07:55 +0100

    7.12.23

---
+++ OOProg-02-Beziehungen/B02_01_AutoFahrerMitfahrer/Test.cs
@@ -0,0 +1,26 @@
+﻿namespace B02_01_AutoFahrerMitfahrer;
+
+class Test {
+    void Run()
+    {
+        Auto auto = new Auto("VW", "Golf", "UE-JK-250");
+
+        Fahrer fahrer = new Fahrer("Max", "Meyer");
+        
+        auto.Einsteigen(fahrer);
+        auto.Print();
+        auto.Aussteigen();
+        auto.Print();
+    }
+
+    static void main()
+    {
+        Test test = new Test();
+        test.Run();
+
+        Console.ReadKey();
+
+    }
+
+}
+
