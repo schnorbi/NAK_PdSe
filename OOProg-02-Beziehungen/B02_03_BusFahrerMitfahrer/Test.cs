6a90867f96633901fec3a82927cddc3c0f220154
Corbian Reiser <corb.reiser@gmail.com>
2023-12-07 10:46:50 +0100

    Bus

---
+++ OOProg-02-Beziehungen/B02_03_BusFahrerMitfahrer/Test.cs
@@ -0,0 +1,36 @@
+﻿namespace B02_03_BusFahrerMitfahrer;
+
+class Test{
+
+    void Run()
+    {
+        Bus bus = new Bus("Mercedes-Benz", "Tourismo", "HH-VV-123");
+
+        Fahrer fahrer = new Fahrer("Max", "Meyer");
+        
+        Mitfahrer mitfahrer1 = new Mitfahrer("Fischer", "Fritz");
+        Mitfahrer mitfahrer2 = new Mitfahrer("Heinrich", "Hans");
+        Mitfahrer mitfahrer3 = new Mitfahrer("Fischer", "Frank");
+        Mitfahrer mitfahrer4 = new Mitfahrer("Horst", "Hans");
+        
+        bus.Einsteigen(fahrer);
+        bus.Print();
+        
+        bus.Einsteigen(mitfahrer1);
+        bus.Einsteigen(mitfahrer2);
+        bus.Einsteigen(mitfahrer3);
+        bus.Einsteigen(mitfahrer4);
+        bus.Print();
+        
+        bus.Aussteigen(mitfahrer3);
+        bus.Print();
+    }
+
+    static void Main()
+    {
+        Test test = new Test();
+        test.Run();
+
+        Console.ReadKey();
+    }
+}
